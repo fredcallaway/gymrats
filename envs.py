c1ac45f35ec872a6cefd368d622783c4cff86d52
fredcallaway <fredc@llaway.com>
2017-02-04 05:06:31 -0800

    fix GridEnv

--- envs.py
+++ envs.py
@@ -6,7 +6,7 @@ import json
 import itertools as it
 import json
 
-from gym import utils
+import gym
 from gym.envs.toy_text.discrete import DiscreteEnv
 from scipy.stats.distributions import bernoulli, norm
 
@@ -20,199 +20,69 @@ EMPTY = 0
 WALL = 1
 GOAL = 2
 
-def clear_screen():
-    print(chr(27) + "[2J")
 
+# One possible result of an action
+# p: probability of this result
+# s1: next state
+# r: reward
+# done: True if the episode is over else False
 Result = namedtuple('Result', ['p', 's1', 'r', 'done'])
 
-class MDP(object):
-    """A markov decision process."""
-
-    def actions(self, state):
-        """Returns a list of actions available at state."""
-        raise NotImplementedError()
-        
-    def results(self, state, action):
-        """Returns a list of possible Results of taking action in state."""
-        raise NotImplementedError()
-    
-    def T(self, s0, a, s1):
-        """Probability of transitioning from state s0 to s1 by action a."""
-        return sum(result.p for result in self.results(s0, a) if result.s1 == s1)
-
-    def R(s, a):
-        """Expected reward from taking action a in state s"""
-        return sum(result.p * result.r for result in self.results(s, a))
-
-
-class EnvMDP(MDP):
-    """An MDP defined by a DiscreteEnv object."""
-    def __init__(self, env):
-        super().__init__()
-        self.env = env
-
-    def actions(self, state):
-        return range(self.env.action_space.n)
-
-    def results(self, state, action):
-        return self.env.P[state][action]
-
-
-class MazeEnv(DiscreteEnv):
-    """Just a maze."""
-    action_names = 'up right down left'.split()
-    _specs = {
-
-    }
-    metadata = {'render.modes': ['human', 'ansi', 'step']}
-
-
-    def __init__(self, spec, is_slippery=True):
-        if spec.endswith('.json'):
-            with open(spec) as f:
-                spec = json.load(f)
-        elif spec in self._specs:
-            spce = self._specs[spec]
-        else:
-            raise ValueError('Bad spec: {}'.format(spec))
-
-        self.goal = tuple(spec['goal'])
-        self.start = tuple(spec.get('start', (1, 1)))
-        M = self.map = np.array(spec['map']).astype(int)
-        M[self.goal] = GOAL
-        
-        self.n_row, self.n_col = n_row, n_col = self.map.shape
-        n_actions = 4
-        n_states = n_row * n_col
-
-        initial_state = np.zeros_like(M)
-        initial_state[self.start] = 1.0
-
-        transition = {s : {a : [] for a in range(n_actions)} for s in range(n_states)}
-
-        def to_s(row, col):
-            return row*n_col + col
-            
-        def inc(row, col, a):
-            if a == UP:
-                row = max(row-1,0)
-            elif a == RIGHT:
-                col = min(col+1,n_col-1)
-            elif a == DOWN:
-                row = min(row+1,n_row-1)
-            elif a == LEFT:
-                col = max(col-1,0)
-            return (row, col)
-
-        for row in range(n_row):
-            for col in range(n_col):
-                s = to_s(row, col)
-                for a in range(n_actions):
-                    if M[row, col] in (WALL, GOAL):
-                        # this state should never be left.
-                        # (probability, nextstate, reward, done)
-                        transition[s][a].append(Result(1.0, s, 0, True))
-                    else:
-                        new_row, new_col = inc(row, col, a)
-                        if M[new_row, new_col] == WALL:
-                            transition[s][a].append(Result(1.0, s, -1, False))
-                        else:
-                            new_state = to_s(new_row, new_col)
-                            done = M[new_row, new_col] == GOAL
-                            rew = 100 if done else -1
-                            transition[s][a].append(Result(1.0, new_state, rew, done))
-
-        super().__init__(n_states, n_actions, transition, initial_state)
-
-    def _render(self, mode='human', close=False):
-        if close:
-            return
-        # if mode == 'step':
-            # input('> ')
-            # clear_screen()
-        outfile = StringIO() if mode == 'ansi' else sys.stdout
-        row, col = self.s // self.n_col, self.s % self.n_col
-        
-        M = self.map.tolist()
-        M[row][col] = 3
-        
-        def block(color):
-            return utils.colorize('  ', color, highlight=True)
-
-        def row_to_string(row):
-            colors = {0: '  ', 1: block('gray'), 2: block('red'), 3: block('blue')}
-            return ''.join(map(colors.get, row))
-
-        outfile.write('\n'.join(row_to_string(row) for row in M) + '\n')
-
-        if self.lastaction is not None:
-            outfile.write("{}\n".format(self.action_names[self.lastaction]))
-        else:
-            outfile.write("\n")
-
-        return outfile
-
-
-class GridMDP(MDP):
-    """Two-dimensional grid."""
-    def __init__(self, n_col=10, n_row=10, seed=None):
-        super().__init__()
-        if seed is not None:
-            np.random.seed(seed)
-        self.grid = np.random.randint(-9, 0, size=n_row * n_col).reshape(n_row, n_col)
-        self.goal = (n_row-1, n_col-1)
-        self.grid[self.goal] = 100
-
-    def actions(self, state):
-        return [UP, RIGHT, DOWN, LEFT]
-
-    def results(self, s0, a):
-        if s0 == self.goal:
-            return [Result(1.0, s0, 0, True)]
-        n_row, n_col = self.grid.shape
-        row, col = s0
-
-        if a == UP:
-            row = max(row-1, 0)
-        elif a == RIGHT:
-            col = min(col+1, n_col-1)
-        elif a == DOWN:
-            row = min(row+1, n_row-1)
-        elif a == LEFT:
-            col = max(col-1,0)
-        
-        s1 = (row, col)
-        r = self.grid[s1]
-        done = True if r > 0 else False
-        return [Result(1.0, s1, r, done)]
 
 
 class GridEnv(DiscreteEnv):
-    """docstring for GridEnv"""
-    def __init__(self, n_col=10, n_row=10, start=(0, 0), goal=None, p_term=0):
+    """A rectangluar grid with random negative rewards and one goal state."""
+    def __init__(self, n_col=10, n_row=10, start=(0, 0), goal=None):
         M = self.grid = np.random.randint(-9, 0, size=n_row * n_col).reshape(n_row, n_col)
+        self.n_col = n_col
+        self.n_row = n_row
+
         n_actions = 4
         n_states = n_row * n_col
+        goal = (n_row - 1, n_col - 1)
 
         initial_state = np.zeros_like(M)
         initial_state[start] = 1.0
 
-        transition = {s : {a : [] for a in range(n_actions)} for s in range(n_states)}
-
-        def to_s(row, col):
-            return row*n_col + col
-
-        def reformat_result(result):
-            return result._replace(s1=to_s(*result.s1))
-
-        for row in range(n_row):
-            for col in range(n_col):
-                for a in range(n_actions):
-                    results = list(map(reformat_result, mdp.results((row,col), a)))
-                    transition[to_s(row, col)][a] = results
+        # transition = {s : {a : [] for a in range(n_actions)} for s in range(n_states)}
 
 
-        super().__init__(n_states, n_actions, transition, initial_state)        
+        def results(s0, a):
+            if s0 == goal:
+                return [Result(1.0, s0, 0, True)]
+            row, col = self.decode(s0)
+
+            if a == UP:
+                row = max(row-1, 0)
+            elif a == RIGHT:
+                col = min(col+1, n_col-1)
+            elif a == DOWN:
+                row = min(row+1, n_row-1)
+            elif a == LEFT:
+                col = max(col-1,0)
+            
+            s1 = self.encode(row, col)
+            r = self.grid[row, col]
+            done = True if r > 0 else False
+            return [Result(1.0, s1, r, done)]
+
+        transition = {s0: {a: results(s0, a) for a in range(n_actions)}
+                      for s0 in range(n_states)}
+
+        # for s0 in range(n_states):
+            # for a in range(n_actions):
+                # results = list(map(reformat_result, results((row,col), a)))
+                # transition[s0][a] = results(s0, a)
+
+
+        super().__init__(n_states, n_actions, transition, initial_state)
+
+    def encode(self, row, col):
+        return self.n_col * row + col
+
+    def decode(self, s):
+        return (s // self.n_col, s % self.n_col)
+
 
 
 class DecisionTreeEnv(DiscreteEnv):
@@ -325,34 +195,6 @@ class DeterministicGraphEnv(DiscreteEnv):
             ('C', [('A', 5), ('B', 0)]),
         )
     }
-
-    specs = {
-        'plane': {
-            'initial': 2,
-            'final': lambda s: 1 / 6,
-            'graph': (
-                # (0, [(5, -3), (3, -7)]),
-                # (1, [(4, 3), (0, 14)]),
-                # (2, [(5, 3), (1, -3)]),
-                # (3, [(2, -3), (1, -7)]),
-                # (4, [(3, -3), (0, 3)]),
-                # (5, [(2, -7), (4, -3)]),
-                (0, [ ( 5, -3 ) , (3, -7 ) ] ) ,
-                (1, [ ( 0, -3  ) , (4, -7 ) ] ) ,
-                (2, [ ( 1, 14  ) , (5, 3 ) ] ) ,
-                (3, [ ( 2, -3 ) , (0, 3 ) ] ) ,
-                (4, [ ( 3, -3 ) , (2, -7  ) ] ) ,
-                (5, [ ( 4, -3 ) , (1, 3 ) ] ) ,
-                # ('william', [('jone', -3), ('clark', -7)]),
-                # ('smith', [('brown', 3), ('william', 14)]),
-                # ('baker', [('jone', 3), ('smith', -3)]),
-                # ('clark', [('baker', -3), ('smith', -7)]),
-                # ('brown', [('clark', -3), ('william', 3)]),
-                # ('jone', [('baker', -7), ('brown', -3)]),
-            )
-        },
-
-    }
     def __init__(self, spec=None):
         if spec is None:
             spec = self.default_spec
@@ -360,8 +202,20 @@ class DeterministicGraphEnv(DiscreteEnv):
             spec = self.specs[spec]
         self.spec = spec
 
-        graph = spec['graph']
+        initial = spec['initial']
         final = spec['final']
+        graph = spec['graph']
+
+        if isinstance(final, dict):
+            final_dict = final
+            final = lambda s: final_dict[s]
+        elif hasattr(final, '__contains__'):
+            final_set = final
+            final = lambda s: int(s in final_set)
+        else:
+            assert callable(final)
+
+
 
         states, all_actions = zip(*graph)
         self.state_idx = {s: i for i, s in enumerate(states)}
@@ -369,7 +223,7 @@ class DeterministicGraphEnv(DiscreteEnv):
         n_actions = max(len(actions) for actions in all_actions)
         
         initial_state = np.zeros(n_states)
-        initial_state[self.state_idx[spec['initial']]] = 1
+        initial_state[self.state_idx[initial]] = 1
 
         # P[s][a] == [(probability, nextstate, reward, done), ...]
         # B default, an action has no effect.
@@ -381,11 +235,18 @@ class DeterministicGraphEnv(DiscreteEnv):
             for a, (s1, r) in enumerate(actions):
                 s1 = self.state_idx[s1]
                 p_term = final(s1)
-                P[s][a] = [(1 - p_term, s1, r, False),
-                           (p_term, s1, r, True)]
+                P[s][a] = []
+                if p_term < 1:
+                    P[s][a].append((1 - p_term, s1, r, False))
+                if p_term > 0:
+                    P[s][a].append((p_term, s1, r, True))
 
         super().__init__(n_states, n_actions, P, initial_state)
 
+    @property
+    def n_states(self):
+        return len(self.state_idx)
+        
     def to_json(self, file):
         with open(file, 'w+') as f:
             json.dump(self.P, f)
@@ -425,20 +286,102 @@ class DeterministicGraphEnv(DiscreteEnv):
         # return spec
 
 
+class MazeEnv(DiscreteEnv):
+    """Just a maze."""
+    action_names = 'up right down left'.split()
+    _specs = {
 
-def n_plan(n, p_term=1/6):
-    spec = {
-        'initial': init,
-        'p_termination': p_term,
-        'graph': (
-            (0, [(5, -3), (3, -7)]),
-            (1, [(0, -3), (4, -7)]),
-            (2, [(1, 14), (5, 3)]),
-            (3, [(2, -3), (0, 3)]),
-            (4, [(3, -3), (2, -7)]),
-            (5, [(4, -3), (1, 3)]),
-        )
     }
+    metadata = {'render.modes': ['human', 'ansi', 'step']}
+
+
+    def __init__(self, spec, is_slippery=True):
+        if spec.endswith('.json'):
+            with open(spec) as f:
+                spec = json.load(f)
+        elif spec in self._specs:
+            spce = self._specs[spec]
+        else:
+            raise ValueError('Bad spec: {}'.format(spec))
+
+        self.goal = tuple(spec['goal'])
+        self.start = tuple(spec.get('start', (1, 1)))
+        M = self.map = np.array(spec['map']).astype(int)
+        M[self.goal] = GOAL
+        
+        self.n_row, self.n_col = n_row, n_col = self.map.shape
+        n_actions = 4
+        n_states = n_row * n_col
+
+        initial_state = np.zeros_like(M)
+        initial_state[self.start] = 1.0
+
+        transition = {s : {a : [] for a in range(n_actions)} for s in range(n_states)}
+
+        def to_s(row, col):
+            return row*n_col + col
+            
+        def inc(row, col, a):
+            if a == UP:
+                row = max(row-1,0)
+            elif a == RIGHT:
+                col = min(col+1,n_col-1)
+            elif a == DOWN:
+                row = min(row+1,n_row-1)
+            elif a == LEFT:
+                col = max(col-1,0)
+            return (row, col)
+
+        for row in range(n_row):
+            for col in range(n_col):
+                s = to_s(row, col)
+                for a in range(n_actions):
+                    if M[row, col] in (WALL, GOAL):
+                        # this state should never be left.
+                        # (probability, nextstate, reward, done)
+                        transition[s][a].append(Result(1.0, s, 0, True))
+                    else:
+                        new_row, new_col = inc(row, col, a)
+                        if M[new_row, new_col] == WALL:
+                            transition[s][a].append(Result(1.0, s, -1, False))
+                        else:
+                            new_state = to_s(new_row, new_col)
+                            done = M[new_row, new_col] == GOAL
+                            rew = 100 if done else -1
+                            transition[s][a].append(Result(1.0, new_state, rew, done))
+
+        super().__init__(n_states, n_actions, transition, initial_state)
+
+    def _render(self, mode='human', close=False):
+        if close:
+            return
+        # if mode == 'step':
+            # input('> ')
+            # clear_screen()
+        outfile = StringIO() if mode == 'ansi' else sys.stdout
+        row, col = self.s // self.n_col, self.s % self.n_col
+        
+        M = self.map.tolist()
+        M[row][col] = 3
+        
+        def block(color):
+            return gym.utils.colorize('  ', color, highlight=True)
+
+        def row_to_string(row):
+            colors = {0: '  ', 1: block('gray'), 2: block('red'), 3: block('blue')}
+            return ''.join(map(colors.get, row))
+
+        outfile.write('\n'.join(row_to_string(row) for row in M) + '\n')
+
+        if self.lastaction is not None:
+            outfile.write("{}\n".format(self.action_names[self.lastaction]))
+        else:
+            outfile.write("\n")
+
+        return outfile
+
+
+
 
 
 def main():
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 
+
 def join(*args, sep=' '):
     return sep.join(map(str, args))
+
 
 def run_episode(agent, env, max_steps=500):
     observation = env.reset()
@@ -15,3 +17,27 @@ def run_episode(agent, env, max_steps=500):
     
     return score, done
 
+
+def logged(condition=lambda r: True):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if condition(result):
+                print(func.__name__, args, kwargs, '->', result)
+            return result
+        return wrapper
+    return decorator
+
+
+
+def clear_screen():
+    print(chr(27) + "[2J")
+
+
+def show_path(env, actions, state=None, render='step'):
+    if state is None:
+        state = env.reset()
+    env.render(mode=render)
+    for a in actions:
+        env.step(a)
+        env.render(mode=render)
